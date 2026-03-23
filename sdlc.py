2a6265299344150a9a541b9575e910a1e1629e25
Prajwalreddy108 <prajwalreddy.eng@gmail.com>
2026-03-23 12:22:11 +0530

    Added sdlc.py

---
+++ sdlc.py
@@ -0,0 +1,11 @@
+print("=== SDLC PROCESS ===")
+
+print("1. Requirement Analysis")
+print("2. Planning")
+print("3. System Design")
+print("4. Development")
+print("5. Testing")
+print("6. Deployment")
+print("7. Maintenance")
+
+print("=== END OF SDLC ===")
